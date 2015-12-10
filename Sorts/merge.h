c8a9e6847de18adeff7bea7576c0f744e16775cf
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-12-10 10:02:19 -0200

    Create merge.h

---
+++ Sorts/merge.h
@@ -0,0 +1,10 @@
+#ifndef _MERGE_H_
+#define _MERGE_H_
+
+
+typedef unsigned int uint;
+
+void MergeSort(int pseq[],uint pbegin,uint pend);
+
+void MergeList(int pseq[],uint pbegin,uint pmiddle,uint pend);
+#endif
